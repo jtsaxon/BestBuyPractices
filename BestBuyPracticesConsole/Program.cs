e8e024c0cf986820f18806aed9059e63d63eefdf
jtsaxon <jaketsaxon@gmail.com>
2020-07-23 13:24:11 -0500

    Add project files.

---
+++ BestBuyPracticesConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BestBuyPracticesConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
